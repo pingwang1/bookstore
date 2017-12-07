47768deda1c92a043c037e34d6d50834cf391cf3
pingwang1 <1274059909@qq.com>
2017-12-07 15:26:43 +0800

    购物车数量的显示

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import cart_add
+from .views import cart_add,cart_count
 
 urlpatterns = [
     url(r'^cart_add/', cart_add,name='cart_add'),
+    url(r'^cart_count/',cart_count,name='cart_count'),
  ]
--- cart/views.py
+++ cart/views.py
@@ -1,17 +1,74 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from books.models import Books
+from django_redis import get_redis_connection
+from utils.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+@csrf_exempt
 def cart_add(request):
 	'''向购物车中添加数据'''
 	#判断用户是否登录
+	data=json.loads(request.body.decode('utf-8'))
 	if not request.session.has_key('islogin'):
-		return JsonResponse({'res':0,'errmsg':'请先登录'})
+		return JsonResponse({'code':0,'errmsg':'请先登录'})
 	#接收数据
-	book_id = request.POST.get('book_id')
-	book_count = request.POST.get('book_count')
+	books_id = data.get('book_id')
+	books_count = data.get('book_count')
+	print(books_id,books_count)
 
 	#进行数据校验
+	if not all([books_id,books_count]):
+		return JsonResponse({'code':1,'errmsg':'数据不完整'})
+	books = Books.objects.get_books_by_id(books_id=books_id)
+	if books is None:
+		#商品不存在
+		return JsonResponse({'code':2,'errmsg':'商品不存在'})
+	try:
+		count = int(books_count)
+		print(count)
+	except Exception as e:
+		#商品数目不合法
+		return JsonResponse({'code':3,'errmsg':'商品数量必须为整数'})
+	#添加商品到购物车
+	#每个用户的购物车记录一条hash数据保存，格式：cart_用户id:商品id 商品数量
+	conn = get_redis_connection('default')
+	cart_key ='cart_%d'%request.session.get('passport_id')
+	res = conn.hget(cart_key,books_id)
+	if res is None:
+		#如果用户的购物车中没有添加该商品，则添加数据
+		res = count
+	else:
+		#如果用户购物车中已经添加过该商品，则累计商品数量
+		res = int(res)+count
+
+	#判断商品的库存
+	if res > books.stock:
+		#库存不足
+		return JsonResponse({'code':4,'errmsg':'库存不足'})
+	else:
+		conn.hset(cart_key,books_id,res)
+
+	#返回结果,成功
+	return JsonResponse({'code':5})
+
+def cart_count(request):
+	'''获取用户购物车里商品数量'''
+	#判断用户是否登录
+	if not request.session.has_key('islogin'):
+		return JsonResponse({'res':0})
+	#计算用户购物车商品数量
+	conn = get_redis_connection('default')
+	cart_key = 'cart_%d'%request.session.get('passport_id')
+	res =0
+	res_list = conn.hvals(cart_key)
+	for i in res_list:
+		res +=int(i)
+	#返回结果
+	return JsonResponse({'res':res})
+
 
 
 
